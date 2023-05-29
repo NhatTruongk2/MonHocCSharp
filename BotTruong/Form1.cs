eb4517c47c2bd23815022cb27851aee8bec5189c
TRUONG <TRUONG@LAPTOP-ECOVO9GP>
2023-05-29 15:51:26 +0700

    bbbb

--- BotTruong/Form1.cs
+++ BotTruong/Form1.cs
@@ -129,7 +129,7 @@ namespace BotTruong
 
                 string messLow = messageText.ToLower(); // Có lẽ k cần thiết!
 
-                // comment
+                // commenthmjyhrte4rwgtehntgerfwdgdf
 
 
                 // ----------- BẮT ĐẦU XỬ LÝ -----------------------------------------------------------------------------
